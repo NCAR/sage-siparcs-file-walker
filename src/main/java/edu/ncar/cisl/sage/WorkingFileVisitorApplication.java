41d0e1b6c446220081cfb0e23ec1191fa83b3d9b
Nathan Hook <nhook@ucar.edu>
2023-06-09 15:49:14 -0600

    Converting tabs to space.  Removed unused import statements.

--- src/main/java/edu/ncar/cisl/sage/WorkingFileVisitorApplication.java
+++ src/main/java/edu/ncar/cisl/sage/WorkingFileVisitorApplication.java
@@ -5,14 +5,10 @@ import edu.ncar.cisl.sage.filewalker.LoggingFileVisitor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import java.nio.file.Path;
 import java.time.Clock;
-import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
@@ -20,26 +16,26 @@ import java.util.List;
 //@ConfigurationProperties(prefix = "config")
 public class WorkingFileVisitorApplication {
 
-	@Value("${walker.startingPath}")
-	private String startingPath;
+    @Value("${walker.startingPath}")
+    private String startingPath;
 
 //	Create Interface for FileVisitor so that multiple can be made and passed in to be used??
 //	Needs to be updated so that it supports FileVisitor<Path> throughout the program ??
 //	@Value("${walker.fileVisitor})
 //	private FileVisitor<Path> fileVisitor
 
-	@Value("${config.ignoredPaths}")
-	private List<String> ignoredPaths;
+    @Value("${config.ignoredPaths}")
+    private List<String> ignoredPaths;
 
-	public static void main(String[] args) {
-		SpringApplication.run(WorkingFileVisitorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WorkingFileVisitorApplication.class, args);
+    }
 
-	@Bean
-	public FileWalker fileWalker() {
+    @Bean
+    public FileWalker fileWalker() {
 
-		LoggingFileVisitor fileVisitor = new LoggingFileVisitor(ignoredPaths);
+        LoggingFileVisitor fileVisitor = new LoggingFileVisitor(ignoredPaths);
 
-		return new FileWalker(Path.of(startingPath), fileVisitor, Clock.systemDefaultZone());
-	}
+        return new FileWalker(Path.of(startingPath), fileVisitor, Clock.systemDefaultZone());
+    }
 }
